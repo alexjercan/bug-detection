a9fcc63a602a2c1b2e4117bf2f6042ffa3cb4922
Alex Jercan <jercan_alex27@yahoo.com>
2023-03-20 16:07:35 +0200

    feat: added execute accuracy

--- codex/main.py
+++ codex/main.py
@@ -4,9 +4,13 @@ import os
 import argparse
 import openai
 import pandas as pd
+import resource
+import subprocess
 import time
 import traceback
+import uuid
 from tqdm.auto import tqdm
+from typing import Optional
 
 LOGGER = logging.getLogger(__name__)
 
@@ -14,10 +18,72 @@ OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 assert OPENAI_API_KEY is not None, "You have to provide an api key for openai"
 
+MAX_VIRTUAL_MEMORY = 100 * 1024 * 1024  # 100 MB
+
 dir_path = os.path.dirname(os.path.abspath(__file__))
 INPUT_PATH = os.path.join(os.path.abspath(os.path.join(dir_path, os.pardir)), "input")
 LOG_PATH = os.path.join(os.path.abspath(os.path.join(dir_path, os.pardir)), "codex.log")
+ROOT_PATH = os.path.join(INPUT_PATH, "Project_CodeNet")
+DERIVED_PATH = os.path.join(ROOT_PATH, "derived")
 GENERATED_PATH = os.path.join(INPUT_PATH, "bugnet")
+
+
+def id2inout(problem_id: str, name: str = "input") -> str:
+    return os.path.join(DERIVED_PATH, "input_output", "data", problem_id, f"{name}.txt")
+
+
+def execute_source(
+    source_code: str,
+    language: str,
+    input: str,
+    timeout: Optional[float] = None,
+) -> Optional[str]:
+    path = f"/tmp/{uuid.uuid4}.xxx"
+    with open(path, "w") as f:
+        f.write(source_code)
+
+    if language == "C++":
+        out = f"/tmp/{uuid.uuid4()}.out"
+
+        result = subprocess.run(["g++", path, "-o", out], capture_output=True)
+
+        if result.returncode != 0:
+            return None
+
+        try:
+            result = subprocess.run(
+                [out],
+                input=input,
+                timeout=timeout,
+                capture_output=True,
+                encoding="utf-8",
+                errors="ignore",
+                preexec_fn=lambda: resource.setrlimit(
+                    resource.RLIMIT_AS, (MAX_VIRTUAL_MEMORY, resource.RLIM_INFINITY)
+                ),
+            )
+            return result.stdout
+        except subprocess.TimeoutExpired:
+            return None
+    if language == "Python":
+        try:
+            result = subprocess.run(
+                ["python3", path],
+                input=input,
+                timeout=timeout,
+                capture_output=True,
+                encoding="utf-8",
+                errors="ignore",
+                preexec_fn=lambda: resource.setrlimit(
+                    resource.RLIMIT_AS, (MAX_VIRTUAL_MEMORY, resource.RLIM_INFINITY)
+                ),
+            )
+
+            return result.stdout
+        except subprocess.TimeoutExpired:
+            return None
+
+    raise NotImplementedError(f"{language} not implemented yet")
 
 
 def make_codex_prompt(pairs_df: pd.DataFrame, source: str, count: int = 5) -> str:
@@ -52,9 +118,8 @@ def generate_codex_results(
                     (submission_pairs_df["language"] == row["language"])
                     & (submission_pairs_df.index != pair_id)
                 ]
+
                 prompt = make_codex_prompt(pairs_df, row["original_src"])
-                print(prompt)
-                exit()
                 response = openai.Completion.create(
                     model="code-davinci-002",
                     prompt=prompt,
@@ -63,8 +128,16 @@ def generate_codex_results(
                     top_p=1,
                 )
                 codex_result = response["choices"][0]["text"]
+
+                with open(id2inout(row["problem_id"], name="input"), "r") as f:
+                    input = f.read()
+                execute_output = execute_source(
+                    codex_result, row["language"], input, timeout=2
+                )
+
                 time.sleep(15)
-                results.append((pair_id, codex_result))
+
+                results.append((pair_id, codex_result, execute_output))
             except Exception as exc:
                 LOGGER.error(
                     f"{pair_id} generated an exception:"
@@ -74,7 +147,9 @@ def generate_codex_results(
                 pbar.set_description(f"[generate codex] processing {pair_id}")
                 pbar.update(1)
 
-    results = pd.DataFrame(results, columns=["index", "codex_predicted"])
+    results = pd.DataFrame(
+        results, columns=["index", "codex_predicted", "execute_output"]
+    )
     results.set_index("index", inplace=True)
     submission_pairs_df = submission_pairs_df.join(results, how="inner")
     submission_pairs_df["codex_predicted"] = submission_pairs_df[
@@ -88,10 +163,18 @@ def generate_codex_results(
 
 def compute_codex_accuracy(submission_pairs_df: pd.DataFrame) -> pd.DataFrame:
     codex_df = submission_pairs_df.copy()
-    codex_df["correct"] = codex_df["codex_predicted"] == codex_df["changed_src"]
+    codex_df["correct_exact"] = codex_df["codex_predicted"] == codex_df["changed_src"]
+
+    def check(row: pd.Series) -> bool:
+        with open(id2inout(row["problem_id"], name="output"), "r") as f:
+            output = f.read()
+
+        return output == row["execute_output"]
+
+    codex_df["correct_execute"] = codex_df.apply(check, axis="columns")
 
     def get_bug_type(row: pd.Series) -> str:
-        line = "\n".join(row["changed_src"].splitlines()[row["j1"]:row["j2"]])
+        line = "\n".join(row["changed_src"].splitlines()[row["j1"] : row["j2"]])
         language = row["language"]
 
         if language == "Python":
@@ -119,19 +202,39 @@ def compute_codex_accuracy(submission_pairs_df: pd.DataFrame) -> pd.DataFrame:
     return codex_df
 
 
-def display_codex_accuracy(codex_df: pd.DataFrame):
-    codex_lang_df = codex_df.groupby("language")["correct"].agg(["sum", "count"])
+def display_codex_accuracy_exact(codex_df: pd.DataFrame):
+    codex_lang_df = codex_df.groupby("language")["correct_exact"].agg(["sum", "count"])
     codex_lang_df["accuracy"] = codex_lang_df["sum"] / codex_lang_df["count"]
 
+    print("Exact match accuracy")
     print(codex_lang_df[["accuracy"]])
 
 
-def display_codex_accuracy_by_type(codex_df: pd.DataFrame):
-    codex_lang_df = codex_df.groupby(["language", "type"])["correct"].agg(
+def display_codex_accuracy_execute(codex_df: pd.DataFrame):
+    codex_lang_df = codex_df.groupby("language")["correct_execute"].agg(["sum", "count"])
+    codex_lang_df["accuracy"] = codex_lang_df["sum"] / codex_lang_df["count"]
+
+    print("Execute accuracy")
+    print(codex_lang_df[["accuracy"]])
+
+
+def display_codex_accuracy_exact_by_type(codex_df: pd.DataFrame):
+    codex_lang_df = codex_df.groupby(["language", "type"])["correct_exact"].agg(
         ["sum", "count"]
     )
     codex_lang_df["accuracy"] = codex_lang_df["sum"] / codex_lang_df["count"]
 
+    print("Exact match accuracy")
+    print(codex_lang_df[["accuracy"]])
+
+
+def display_codex_accuracy_execute_by_type(codex_df: pd.DataFrame):
+    codex_lang_df = codex_df.groupby(["language", "type"])["correct_execute"].agg(
+        ["sum", "count"]
+    )
+    codex_lang_df["accuracy"] = codex_lang_df["sum"] / codex_lang_df["count"]
+
+    print("Execute accuracy")
     print(codex_lang_df[["accuracy"]])
 
 
@@ -178,5 +281,7 @@ if __name__ == "__main__":
 
     codex_df = generate_codex_results(submission_pairs_df)
     codex_df = compute_codex_accuracy(codex_df)
-    display_codex_accuracy(codex_df)
-    display_codex_accuracy_by_type(codex_df)
+    display_codex_accuracy_exact(codex_df)
+    display_codex_accuracy_execute(codex_df)
+    display_codex_accuracy_exact_by_type(codex_df)
+    display_codex_accuracy_execute_by_type(codex_df)
