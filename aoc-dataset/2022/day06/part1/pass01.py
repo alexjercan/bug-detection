23bdf0f04bd2af980a6d29be95ebb30c0c16fced
Alex Jercan <jercan_alex27@yahoo.com>
2023-06-06 16:33:25 +0300

    fix

--- aoc-dataset/2022/day06/part1/pass01.py
+++ aoc-dataset/2022/day06/part1/pass01.py
@@ -1,5 +1,5 @@
 def solve(test: str) -> str:
-    for i in range(len(test) - 4):
+    for i in range(len(test) - 3):
         if len(set(test[i : i + 4])) == 4:
             break
 
--- aoc-dataset/2022/day06/part2/pass01.py
+++ aoc-dataset/2022/day06/part2/pass01.py
@@ -1,5 +1,5 @@
 def solve(test: str) -> str:
-    for i in range(len(test) - 14):
+    for i in range(len(test) - 13):
         if len(set(test[i : i + 14])) == 14:
             break
 
